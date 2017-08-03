9769dffe2d057bdce11333f0e036e73ae22d4bf3
chenjian <chenjian@kankan.com>
2017-08-03 20:39:13 +0800

    增加Camera的控制相关代码

--- app/src/main/java/com/example/cj/videoeditor/camera/CameraController.java
+++ app/src/main/java/com/example/cj/videoeditor/camera/CameraController.java
@@ -4,15 +4,29 @@ import android.graphics.Point;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by cj on 2017/8/2.
  * desc 相机的管理类
  */
 
 public class CameraController implements ICamera{
-
+    /**相机的宽高及比例配置*/
     private ICamera.Config mConfig;
+    /**相机实体*/
     private Camera mCamera;
+    /**预览的尺寸*/
+    private Camera.Size preSize;
+    /**实际的尺寸*/
+    private Camera.Size picSize;
+
+    private Point mPreSize ;
+    private Point mPicSize ;
+
 
     public CameraController(){
         /**初始化一个默认的格式大小*/
@@ -24,40 +38,123 @@ public class CameraController implements ICamera{
 
     public void open(int cameraId) {
         mCamera = Camera.open(cameraId);
+        if (mCamera != null){
+            /**选择当前设备允许的预览尺寸*/
+            Camera.Parameters param = mCamera.getParameters();
+            preSize = getPropPreviewSize(param.getSupportedPreviewSizes(), mConfig.rate,
+                    mConfig.minPreviewWidth);
+            picSize = getPropPictureSize(param.getSupportedPictureSizes(),mConfig.rate,
+                    mConfig.minPictureWidth);
+            param.setPictureSize(picSize.width, picSize.height);
+            param.setPreviewSize(preSize.width,preSize.height);
+
+            mCamera.setParameters(param);
+            Camera.Size pre=param.getPreviewSize();
+            Camera.Size pic=param.getPictureSize();
+            mPicSize=new Point(pic.height,pic.width);
+            mPreSize=new Point(pre.height,pre.width);
+        }
     }
 
     @Override
     public void setPreviewTexture(SurfaceTexture texture) {
-
+        if(mCamera!=null){
+            try {
+                mCamera.setPreviewTexture(texture);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void setConfig(Config config) {
-
+        this.mConfig=config;
     }
 
     @Override
-    public void setOnPreviewFrameCallback(PreviewFrameCallback callback) {
-
+    public void setOnPreviewFrameCallback(final PreviewFrameCallback callback) {
+        if(mCamera!=null){
+            mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+                @Override
+                public void onPreviewFrame(byte[] data, Camera camera) {
+                    callback.onPreviewFrame(data,mPreSize.x,mPreSize.y);
+                }
+            });
+        }
     }
 
     @Override
     public void preview() {
-
+        if (mCamera != null){
+            mCamera.startPreview();
+        }
     }
 
     @Override
     public Point getPreviewSize() {
-        return null;
+        return mPreSize;
     }
 
     @Override
     public Point getPictureSize() {
-        return null;
+        return mPicSize;
     }
 
     @Override
     public boolean close() {
+        if (mCamera != null){
+            mCamera.stopPreview();
+            mCamera.release();
+        }
         return false;
     }
+    private Camera.Size getPropPictureSize(List<Camera.Size> list, float th, int minWidth){
+        Collections.sort(list, sizeComparator);
+        int i = 0;
+        for(Camera.Size s:list){
+            if((s.height >= minWidth) && equalRate(s, th)){
+                break;
+            }
+            i++;
+        }
+        if(i == list.size()){
+            i = 0;
+        }
+        return list.get(i);
+    }
+    private Camera.Size getPropPreviewSize(List<Camera.Size> list, float th, int minWidth){
+        Collections.sort(list, sizeComparator);
+
+        int i = 0;
+        for(Camera.Size s:list){
+            if((s.height >= minWidth) && equalRate(s, th)){
+                break;
+            }
+            i++;
+        }
+        if(i == list.size()){
+            i = 0;
+        }
+        return list.get(i);
+    }
+    private static boolean equalRate(Camera.Size s, float rate){
+        float r = (float)(s.width)/(float)(s.height);
+        if(Math.abs(r - rate) <= 0.03) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    private Comparator<Camera.Size> sizeComparator=new Comparator<Camera.Size>(){
+        public int compare(Camera.Size lhs, Camera.Size rhs) {
+            if(lhs.height == rhs.height){
+                return 0;
+            }else if(lhs.height > rhs.height){
+                return 1;
+            }else{
+                return -1;
+            }
+        }
+    };
 }
